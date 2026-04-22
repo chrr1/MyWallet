51434e2da85626f3340eb07a8bb691f0fe19bb18
chrr1 <adhikchris123@gmail.com>
2026-04-22 08:51:06 +0700

    server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
+
+const app = express(); 
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/",(req,res) => {
-    res.send("Yoiihh Jalan Brokk");
+    res.send("Yoiihh Jalan Brokk anjay");
 });
 
-app.listen(5001,() => {
-    console.log("Server is up and running on PORT:5001")
+app.listen(PORT,() => {
+    console.log("Server is up and running on PORT:", PORT);
 }); 
